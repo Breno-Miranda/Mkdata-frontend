b60f141fba6330b1691a0835e36b82e760336806
Breno Miranda <brenomirandaster@gmail.com>
2021-01-17 15:46:45 -0300

    import modules common

--- src/app/layouts/site/site.module.ts
+++ src/app/layouts/site/site.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ModulesModule } from 'src/app/modules/modules.module';
+import { SiteRoutes } from './site.routing.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(SiteRoutes),
+    NgModule,
+    ModulesModule,
+    FormsModule,
   ]
 })
 export class SiteModule { }
