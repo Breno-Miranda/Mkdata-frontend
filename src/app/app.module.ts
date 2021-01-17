590e6e6eb5b05d0dd8285813e53685d1026b75fa
Breno Miranda <brenomirandaster@gmail.com>
2021-01-17 15:36:16 -0300

    adding router layout site and module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LayoutsComponent } from './layouts/layouts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/layouts/site/site.module.ts
+++ src/app/layouts/site/site.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SiteComponent } from './site/site.component';
 
 
 
 @NgModule({
-  declarations: [SiteComponent],
+  declarations: [],
   imports: [
     CommonModule
   ]
 })
-export class LayoutsModule { }
+export class SiteModule { }
