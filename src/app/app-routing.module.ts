cd520a4f271545baf3402e44261402af7f322ee0
Breno Miranda <brenomirandaster@gmail.com>
2021-01-17 15:57:16 -0300

    adding router default (layout content)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+import { SiteComponent } from './layouts/site/site.component';
+
+
+const routes: Routes = [
+  {
+    path: '', component: SiteComponent,
+    children: [
+      { path: '', loadChildren:  () => import('src/app/layouts/site/site.module').then(m => m.SiteModule)}
+    ]
+  },
+  { path: '**', redirectTo: '' }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
