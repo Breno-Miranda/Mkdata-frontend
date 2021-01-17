c93044e5178c78544b590902d15c33812dcfe91a
Breno Miranda <brenomirandaster@gmail.com>
2021-01-17 18:22:26 -0300

    adding new router (edit and del)

--- src/app/layouts/site/site.routing.module.ts
+++ src/app/layouts/site/site.routing.module.ts
@@ -1,14 +1,20 @@
 import { Routes } from '@angular/router';
+
+// home
+import { SiteComponent } from '../../modules/site/site.component';
+
+// outher page
 import { AddEditComponent } from 'src/app/modules/clients/add-edit/add-edit.component';
 import { ClientsComponent } from 'src/app/modules/clients/clients.component';
-
-import { SiteComponent } from '../../modules/site/site.component';
+import { DelComponent } from 'src/app/modules/clients/del/del.component';
 
 
 export const SiteRoutes: Routes = [
   {path: '', component: SiteComponent},
   {path: 'clientes', component: ClientsComponent},
   {path: 'clientes/adicionar', component: AddEditComponent},
+  {path: 'clientes/editar/:id', component: AddEditComponent},
+  {path: 'clientes/editar/:id', component: DelComponent},
 ];
 
 
