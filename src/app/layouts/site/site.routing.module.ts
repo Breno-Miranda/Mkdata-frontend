0fd0d8747297ea0165d1fe282383da3ba3c2a983
Breno Miranda <brenomirandaster@gmail.com>
2021-01-17 17:58:16 -0300

    develop page clients

--- src/app/layouts/site/site.routing.module.ts
+++ src/app/layouts/site/site.routing.module.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
+import { ClientsComponent } from 'src/app/modules/clients/clients.component';
+
 import { SiteComponent } from '../../modules/site/site.component';
 
 
 export const SiteRoutes: Routes = [
   {path: '', component: SiteComponent},
+  {path: 'clientes', component: ClientsComponent},
 ];
 
 
